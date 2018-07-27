8a3ebb3b82ae6c567220cff57d08583ee36e0cd0
Connie Leung <cleung334@gmail.com>
2018-07-27 08:15:34 +0800

    initial commit
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ src/app/models/connect.enum.ts
@@ -0,0 +1,14 @@
+export enum Player {
+    PLAYER1 = '1',
+    PLAYER2 = '2',
+    COMPUTER = 'c'
+}
+
+export enum Outcome {
+    PLAYER1_WINS = 'Player 1 wins!!!',
+    PLAYER2_WINS = 'Player 2 wins!!!',
+    COMPUTER_WINS = 'Computer wins!!!',
+    DRAW = 'Draw!!!',
+    NEW_GAME = ''
+}
+
