cc1123111c8135355094807f6fd6528f450bf876
Connie Leung <cleung334@gmail.com>
2018-07-28 11:52:20 +0800

    Fix vertical checking and horizontal checking in isWinningPiece function

--- src/app/util/grid.util.spec.ts
+++ src/app/util/grid.util.spec.ts
@@ -1,15 +1,275 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GridUtil } from './grid.util';
+import { ROWS, COLUMNS, FREE_CELL, Player } from '../models/index';
 
 describe('GridUtil', () => {
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({
-  //     providers: [GridService]
-  //   });
-  // });
+  let gridUtil = new GridUtil();  
+  let grid = [];
+  beforeAll(() => {
+    for (let i = 0; i < ROWS * COLUMNS; i++) {
+      grid.push(FREE_CELL); 
+    }
+    gridUtil.setGrid(grid);
+  });
 
-  // it('should be created', inject([GridService], (service: GridService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  describe('canPlay returns true for all columns in a new grid', () => {
+    it('all columns can play when grid is empty', () => {
+      for (let i = 0; i < COLUMNS; i++) {
+        expect(gridUtil.canPlay(i)).toBe(true);
+      }
+    });
+
+    it('all columns have height 0', () => {
+      for (let i = 0; i < COLUMNS; i++) {
+        expect(gridUtil.height[i]).toBe(0);
+      }
+    });
+
+    it('zero move is made', () => {
+        expect(gridUtil.numMoves).toBe(0);
+    });
+  });
+
+  describe('canPlay returns true if the column has vacant cell', () => {
+    beforeEach(() => {
+      let cloneGrid = JSON.parse(JSON.stringify(grid));
+      [0,5,8,10,19,22,24,33,36].forEach((i) => cloneGrid[i] = Player.PLAYER2);
+      [1,3,4,12,15,17,26,29].forEach((i) => cloneGrid[i] = Player.PLAYER1);
+      gridUtil.setGrid(cloneGrid);
+    });
+
+    it('columns can play', () => {
+      expect(gridUtil.canPlay(0)).toBe(true);
+      expect(gridUtil.canPlay(2)).toBe(true);
+      expect(gridUtil.canPlay(3)).toBe(true);
+      expect(gridUtil.canPlay(4)).toBe(true);
+      expect(gridUtil.canPlay(5)).toBe(true);
+      expect(gridUtil.canPlay(6)).toBe(true);
+    });
+
+    it('column cannot play', () => {
+      expect(gridUtil.canPlay(1)).toBe(false);
+    });
+
+    it('columns have the correct height', () => {
+      expect(gridUtil.height[0]).toBe(1);
+      expect(gridUtil.height[1]).toBe(6);
+      expect(gridUtil.height[2]).toBe(0);
+      expect(gridUtil.height[3]).toBe(4);
+      expect(gridUtil.height[4]).toBe(1);
+      expect(gridUtil.height[5]).toBe(5);
+      expect(gridUtil.height[6]).toBe(0);
+    });
+
+    it('grid has correct number of moves', () => {
+      expect(gridUtil.numMoves).toBe(17);
+    });
+  });
+
+  describe('play updates column height and grid cell with player\'s piece', () => {
+    beforeEach(() => {
+      for (let i = 0; i < ROWS * COLUMNS; i++) {
+        grid.push(FREE_CELL); 
+      }
+      gridUtil.setGrid(grid);
+    });
+
+    it('Add a single piece to grid', () => {
+      gridUtil.play(0, Player.PLAYER1);
+      expect(gridUtil.height[0]).toBe(1);
+      expect(gridUtil.numMoves).toBe(1);
+    });
+
+    it('Add multiple pieces to the same column', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      expect(gridUtil.height[6]).toBe(4);
+      expect(gridUtil.numMoves).toBe(4);
+    });
+
+    it('Add multiple pieces to the same column until it is filled entirely', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      expect(gridUtil.height[6]).toBe(6);
+      expect(gridUtil.numMoves).toBe(6);
+    });
+
+    it('Add multiple pieces in different columns', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+
+      gridUtil.play(3, Player.PLAYER1);
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(3, Player.PLAYER1);      
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(4, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+
+      gridUtil.play(5, Player.PLAYER1);
+      expect(gridUtil.height[0]).toBe(0);
+      expect(gridUtil.height[1]).toBe(1);
+      expect(gridUtil.height[2]).toBe(0);
+      expect(gridUtil.height[3]).toBe(2);
+      expect(gridUtil.height[4]).toBe(3);
+      expect(gridUtil.height[5]).toBe(1);
+      expect(gridUtil.height[6]).toBe(2);
+
+      expect(gridUtil.numMoves).toBe(9);
+    });
+  });
+
+  describe('play updates column height and grid cell with player\'s piece', () => {
+    beforeEach(() => {
+      for (let i = 0; i < ROWS * COLUMNS; i++) {
+        grid.push(FREE_CELL); 
+      }
+      gridUtil.setGrid(grid);
+    });
+
+    it('Add a single piece to grid', () => {
+      gridUtil.play(0, Player.PLAYER1);
+      expect(gridUtil.height[0]).toBe(1);
+      expect(gridUtil.numMoves).toBe(1);
+    });
+
+    it('Add multiple pieces to the same column', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      expect(gridUtil.height[6]).toBe(4);
+      expect(gridUtil.numMoves).toBe(4);
+    });
+
+    it('Add multiple pieces to the same column until it is filled entirely', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+      gridUtil.play(6, Player.PLAYER1);
+      expect(gridUtil.height[6]).toBe(6);
+      expect(gridUtil.numMoves).toBe(6);
+    });
+
+    it('Add multiple pieces in different columns', () => {
+      gridUtil.play(6, Player.PLAYER1);
+      gridUtil.play(6, Player.PLAYER2);
+
+      gridUtil.play(3, Player.PLAYER1);
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(3, Player.PLAYER1);      
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(4, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+
+      gridUtil.play(5, Player.PLAYER1);
+      expect(gridUtil.height[0]).toBe(0);
+      expect(gridUtil.height[1]).toBe(1);
+      expect(gridUtil.height[2]).toBe(0);
+      expect(gridUtil.height[3]).toBe(2);
+      expect(gridUtil.height[4]).toBe(3);
+      expect(gridUtil.height[5]).toBe(1);
+      expect(gridUtil.height[6]).toBe(2);
+
+      expect(gridUtil.numMoves).toBe(9);
+    });
+  });
+
+  describe('isWinningMove return true if 4 pieces are connected together vertically', () => {
+    beforeEach(() => {
+      grid = [];
+      for (let i = 0; i < ROWS * COLUMNS; i++) {
+        grid.push(FREE_CELL); 
+      }
+      gridUtil.setGrid(grid);
+    });
+
+    it('return true if it is vertically connected', () => {
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      expect(gridUtil.isWinningMove(0, Player.PLAYER1)).toBe(true);
+    });
+
+    it('return true if it is vertically connected 2', () => {
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(0, Player.PLAYER2);
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      gridUtil.play(0, Player.PLAYER1);
+      gridUtil.play(1, Player.PLAYER2);
+      expect(gridUtil.isWinningMove(0, Player.PLAYER1)).toBe(true);
+    });
+
+    it('return true if it is vertically connected 3', () => {
+      gridUtil.play(4, Player.PLAYER1);
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(5, Player.PLAYER1);
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(5, Player.PLAYER1);
+      gridUtil.play(4, Player.PLAYER2);
+      gridUtil.play(5, Player.PLAYER1);
+      expect(gridUtil.isWinningMove(4, Player.PLAYER2)).toBe(true);
+    });
+  });
+
+  describe('isWinningMove return true if 4 pieces are connected together horizontally', () => {
+    beforeEach(() => {
+      grid = [];
+      for (let i = 0; i < ROWS * COLUMNS; i++) {
+        grid.push(FREE_CELL); 
+      }
+      gridUtil.setGrid(grid);
+    });
+
+    it('return true if it is horizontally connected and the winning piece is the second one of the connected 4', () => { 
+      [0,2,4,5,11].forEach((i) => grid[i] = Player.PLAYER1);
+      [1,3,7,8,10].forEach((i) => grid[i] = Player.PLAYER2);
+      gridUtil.setGrid(grid);
+      expect(gridUtil.isWinningMove(2, Player.PLAYER2)).toBe(true);
+    });
+
+    it('return true if it is horizontally connected and the winning piece is the third one of the connected 4', () => {
+      [1,3,5,6,13,20].forEach((i) => grid[i] = Player.PLAYER1);
+      [2,4,8,9,11].forEach((i) => grid[i] = Player.PLAYER2);
+      gridUtil.setGrid(grid);
+      expect(gridUtil.isWinningMove(3, Player.PLAYER2)).toBe(true);
+    });
+
+    it('return true if it is horizontally connected and the winning piece is the last one of the connected 4', () => {
+      [3,5,10,11,12].forEach((i) => grid[i] = Player.PLAYER1);
+      [4,6,17,18,19].forEach((i) => grid[i] = Player.PLAYER2);
+      gridUtil.setGrid(grid);
+      expect(gridUtil.isWinningMove(6, Player.PLAYER1)).toBe(true);
+    });
+
+    it('return true if it is horizontally connected and the winning piece is the first one of the connected 4', () => {
+      [1,3,4,5,12,19].forEach((i) => grid[i] = Player.PLAYER1);
+      [2,6,9,10,11].forEach((i) => grid[i] = Player.PLAYER2);
+      gridUtil.setGrid(grid);
+      expect(gridUtil.isWinningMove(1, Player.PLAYER2)).toBe(true);
+    });
+
+    it('return true if it is horizontally connected and it is actually connect 5', () => {
+      [1,3,4,5,15,19,22].forEach((i) => grid[i] = Player.PLAYER1);
+      [2,8,9,11,12,18].forEach((i) => grid[i] = Player.PLAYER2);
+      gridUtil.setGrid(grid);
+      expect(gridUtil.isWinningMove(3, Player.PLAYER2)).toBe(true);
+    });
+  });
 });
--- src/app/util/grid.util.ts
+++ src/app/util/grid.util.ts
@@ -1,16 +1,153 @@
+import { FREE_CELL, ROWS, COLUMNS } from '../models/';
+
 export class GridUtil {
+  private grid: string[];
+  private _height: number[] = [];
 
-  constructor(grid: number[]) { }
+  constructor() {
+    for (let i = 0; i < COLUMNS; i++) {
+      this._height.push(0);
+    }
+  }
 
+  setGrid(grid: string[]) {
+    const convertIdxToRowCol = (idx) => {
+      const col = idx % COLUMNS;
+      const row = (idx - col) / ROWS;
+      return { row, col };
+    };
+
+    this.grid = [];
+    this._height = [];
+    for (let i = 0; i < COLUMNS; i++) {
+      this._height.push(0);
+    }
+   // console.log('0', grid.length);
+    for (let i = 0; i < grid.length; i++) {
+      const { row, col } = convertIdxToRowCol(i);
+      // console.log(row, col);
+      this.grid.push(grid[i]);
+      if (grid[i] !== FREE_CELL) {
+        this._height[col]++;
+      }
+    }
+  }
+
+  convertRowColToIdx(row, col) {
+    if (row >= 0 && row < ROWS && col >= 0 && col < COLUMNS) {
+        return row * COLUMNS + col;
+    }
+    return -1;
+  }
+  
   canPlay(column) {
-
+    if (column < 0 || column >= COLUMNS) {
+      return false;
+    }
+    return this._height[column] < ROWS;
   }
 
   isWinningMove(column, player) {
+    if (!this.canPlay(column)) {
+      console.error(`Cannot put a piece on ${column}`);
+      return false;
+    }
+    
+    // check vertical
+    if (this.height[column] >= 3) {
+      const idx1 = this.convertRowColToIdx(this.height[column] - 1, column);
+      const idx2 = this.convertRowColToIdx(this.height[column] - 2, column);
+      const idx3 =this.convertRowColToIdx(this.height[column] - 3, column);
+      if (this.grid[idx1] === player && this.grid[idx2] === player && this.grid[idx3] === player) {
+        console.log(`Win vertically. height[column]: ${this._height[column]}, column: ${column}`);
+        return true;
+      }
+    }
 
+    // check horizontal
+    for (let x = 3; x >= 0; x--) {
+        let pieces = 0;
+        for (let delta = -3; delta <= 0; delta++) {
+            if (delta !== -x) {
+                const colIdx = column + delta + x;
+                if (colIdx >= 0 && colIdx < COLUMNS) {
+                  let idx = this.convertRowColToIdx(this._height[column], colIdx);
+                    if (this.grid[idx] === player) {
+                        pieces += 1;
+                        console.log (`Yes: x: ${x}, delte: ${delta}, colIdx: ${colIdx}, potentialRowIdx: ${this._height[column]}, player: ${player}, actual: ${this.grid[idx]}`);
+                      } else {
+                      console.log (`Nope: x: ${x}, delte: ${delta}, colIdx: ${colIdx}, potentialRowIdx: ${this._height[column]}, player: ${player}, actual: ${this.grid[idx]}`);
+                    }    
+                }            
+            }
+        }
+        console.log('-----------------------------------------------');
+        if (pieces === 3) {
+          console.log (`win combo: x ${-x}, height[column]: ${this._height[column]}, column: ${column}`);
+          return true;
+        }
+    }
+
+    // check left diagonal
+    // for (let x = 3; x >= 0; x--) {
+    //     let pieces = 0;
+    //     for (let delta = -3; delta <= 0; delta++) {
+    //         if (delta !== x) {
+    //             const colIdx = column + delta + x;
+    //             const rowIdx = this.height[column] - (delta + x);                    
+    //             if (colIdx >= 0 && colIdx < COLUMNS && rowIdx >= 0 && rowIdx < ROWS) {
+    //               const idx = this.convertRowColToIdx(rowIdx - colIdx, colIdx);
+    //               if (this.grid[idx] === player) {
+    //                   pieces += 1;
+    //               } 
+    //             }            
+    //         }
+    //     }
+    //     if (pieces === 3) {
+    //         return true;
+    //     }
+    // }
+
+    // // check right diagonal
+    // for (let x = 3; x >= 0; x--) {
+    //   let pieces = 0;
+    //   for (let delta = -3; delta <= 0; delta++) {
+    //       if (delta !== x) {
+    //           const colIdx = column + delta + x;
+    //           const rowIdx = this.height[column] + (delta + x);                   
+    //           if (colIdx >= 0 && colIdx < COLUMNS && rowIdx >= 0 && rowIdx < ROWS) {
+    //               const idx = this.convertRowColToIdx(rowIdx - colIdx, colIdx);
+    //               if (this.grid[idx] === player) {
+    //                 pieces += 1;
+    //               }    
+    //           }        
+    //       }
+    //   }
+    //   if (pieces === 3) {
+    //       return true;
+    //   }
+    // }
+    return false;
   }
 
   play(column, player) {
+    if (!this.canPlay(column)) {
+      return;
+    }
+    const idx = this.convertRowColToIdx(this.height[column], column);
+    this.grid[idx] = player;
+    this._height[column]++;
+  }
 
+  get height() {
+    const heightCopy = [];
+    for (let i = 0; i < COLUMNS; i++) {
+      heightCopy.push(this._height[i]);
+    }
+    return heightCopy;
+  }
+  
+  get numMoves() {
+    return this.grid.filter((c) => c !== FREE_CELL).length;
   }
 }
