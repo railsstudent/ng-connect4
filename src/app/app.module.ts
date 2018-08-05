771fe9a59ed8f864a3fa22b290480ebe0c9ae3f0
Connie Leung <cleung334@gmail.com>
2018-08-05 16:20:10 +0800

    Remove ngrx/effect
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
+import { AppComponent } from "./app.component";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./reducers";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument({
-      name: 'NgRx Connect 4 Store DevTools',
-      maxAge: 25 //  Retains last 25 states
-    }) : [],
+    !environment.production
+      ? StoreDevtoolsModule.instrument({
+          name: "NgRx Connect 4 Store DevTools",
+          maxAge: 25 //  Retains last 25 states
+        })
+      : []
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
