3c42fa04438ee7002f9b6eaeae06164cee574772
Connie Leung <cleung334@gmail.com>
2018-08-12 19:53:59 +0800

    Add basic e2e test cases
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,58 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display enter game message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getShellTitle()).toEqual('Enter game of Connect Four');
+  });
+
+  it('should display show Human vs Human button', () => {
+    page.navigateTo();
+    expect(page.getTwoPlayersButton().getText()).toEqual('Human vs Human');
+  });
+
+  it('should display show Human vs Computer button', () => {
+    page.navigateTo();
+    expect(page.getOnePlayerButton().getText()).toEqual('Human vs Computer');
+  });
+
+  it('should show the game after clicking Human vs Human button', async () => {
+    page.navigateTo();
+    page.refresh();
+    const btn = page.getTwoPlayersButton();
+    await btn.click();
+    expect(page.getFirstPlayerName()).toEqual('Player 1');
+    expect(page.getFirstPlayerColor()).toEqual('rgba(255, 0, 0, 1)');
+    expect(page.getFirstPlayerTurn().getText()).toEqual(['Your Turn.', 'Please make a move']);
+
+    expect(page.getSecondPlayerName()).toEqual('Player 2');
+    expect(page.getSecondPlayerColor()).toEqual('rgba(255, 255, 0, 1)');
+    expect(await page.getSecondPlayerTurn().isPresent()).toBeFalsy();
+
+    expect(page.getBoardTitle()).toBe('Connect Four Game');
+    expect(page.getBoardMovesLeftLabel()).toBe('Remaining Moves:');
+    expect(page.getBoardMovesLeftContent()).toEqual('42');
+    expect(await page.getBoardGrid().isPresent()).toBeTruthy();
+    expect(await page.getBoardSelectionColumns().isPresent()).toBeTruthy();
+  });
+
+  it('should show the game after clicking Human vs Computer button', async () => {
+    page.navigateTo();
+    page.refresh();
+    const btn = page.getOnePlayerButton();
+    await btn.click();
+    expect(page.getFirstPlayerName()).toEqual('Player 1');
+    expect(page.getFirstPlayerColor()).toEqual('rgba(255, 0, 0, 1)');
+    expect(page.getFirstPlayerTurn().getText()).toEqual(['Your Turn.', 'Please make a move']);
+
+    expect(page.getSecondPlayerName()).toEqual('Computer');
+    expect(page.getSecondPlayerColor()).toEqual('rgba(255, 0, 255, 1)');
+    expect(await page.getSecondPlayerTurn().isPresent()).toBeFalsy();
+
+    expect(page.getBoardTitle()).toBe('Connect Four Game');
+    expect(page.getBoardMovesLeftLabel()).toBe('Remaining Moves:');
+    expect(page.getBoardMovesLeftContent()).toEqual('42');
+    expect(await page.getBoardGrid().isPresent()).toBeTruthy();
+    expect(await page.getBoardSelectionColumns().isPresent()).toBeTruthy();
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,79 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('connect-root h1')).getText();
+  refresh() {
+    return browser.refresh();
+  }
+
+  getShellTitle() {
+    return element(by.css('connect-root .shell-title')).getText();
+  }
+
+  getTwoPlayersButton() {
+    return element(by.css('connect-root .btn-human'));
+  }
+
+  getOnePlayerButton() {
+    return element(by.css('connect-root .btn-computer'));
+  }
+
+  getFirstPlayer() {
+    return element(by.tagName('connect-player:nth-of-type(1)'));
+  }
+
+  getSecondPlayer() {
+    return element(by.tagName('connect-player:nth-of-type(2)'));
+  }
+
+  getFirstPlayerName() {
+    return this.getFirstPlayer().$('.name').getText();
+  }
+
+  getFirstPlayerColor() {
+    return this.getFirstPlayer().$('.piece').getCssValue('background-color');
+  }
+
+  getFirstPlayerTurn() {
+    return this.getFirstPlayer().$$('.turn');
+  }
+
+  getSecondPlayerName() {
+    return this.getSecondPlayer().$('.name').getText();
+  }
+
+  getSecondPlayerColor() {
+    return this.getSecondPlayer().$('.piece').getCssValue('background-color');
+  }
+
+  getSecondPlayerTurn() {
+    return this.getSecondPlayer().$$('.turn');
+  }
+
+  getBoard() {
+    return element(by.tagName('connect-board'));
+  }
+
+  getBoardTitle() {
+    return this.getBoard().$('.board-title').getText();
+  }
+
+  getBoardMovesLeftLabel() {
+    return this.getBoard().$('.moves-left-label').getText();
+  }
+
+  getBoardMovesLeftContent() {
+    return this.getBoard().$('.moves-left-content').getText();
+  }
+
+  getBoardGrid(): ElementFinder {
+    return this.getBoard().$('.grid-container');
+  }
+
+  getBoardSelectionColumns(): ElementFinder {
+    return this.getBoard().$('.select-column-container');
   }
 }
