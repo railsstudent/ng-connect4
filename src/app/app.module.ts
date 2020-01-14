5a887d279bca2309bc1eed783a95f62e11c78fb4
Connie Leung <connie.leung@diginex.com>
2020-01-14 12:28:24 +0800

    chore: upgrade to ngrx 8
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
-import { AppComponent } from "./app.component";
-import { StoreModule } from "@ngrx/store";
-import { reducers, metaReducers } from "./reducers";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { environment } from "../environments/environment";
+import { BrowserModule } from "@angular/platform-browser";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-
-import { PlayerComponent } from "./player/player.component";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
 import { BoardComponent } from "./board/board.component";
+import { PlayerComponent } from "./player/player.component";
+import { metaReducers, reducers } from "./reducers";
 import { ShellComponent } from "./shell/shell.component";
 
 // Add an icon to the library for convenient access in other components
