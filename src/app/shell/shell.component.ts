ff8a04e63846aa9b1252e10072b37560ce712ae6
Connie Leung <cleung334@gmail.com>
2020-01-28 15:50:41 +0800

    feat: custom modal for human vs computer mode
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/shell/shell.component.ts
+++ src/app/shell/shell.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from "@angular/forms";
+import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 import { Mode, Player, PieceColor } from "../models";
 import { Store, select } from "@ngrx/store";
-import { AppState, selectMode, selectResetGame } from "../reducers";
+import { AppState, selectMode } from "../reducers";
 import { ModalService } from "../shared/modal";
 
 const PLAYER_ONE = "Player 1";
 const PLAYER_TWO = "Player 2";
+const COMPUTER = "Computer";
 
 @Component({
   selector: "connect-shell",
@@ -21,18 +22,21 @@ export class ShellComponent implements OnInit {
   mode = Mode.UNKNOWN;
   isUnknown = true;
 
-  form: FormGroup;
+  formHuman: FormGroup;
+
   strPlayerOneName = PLAYER_ONE;
   strPlayerTwoName = PLAYER_TWO;
+  strComputerName = COMPUTER;
 
   mode$ = this.store.pipe(select(selectMode));
 
   constructor(private store: Store<AppState>, private modalService: ModalService, private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.form = this.fb.group({
+    this.formHuman = this.fb.group({
       playerOneName: new FormControl(this.strPlayerOneName, { validators: [Validators.required], updateOn: "change" }),
       playerTwoName: new FormControl(this.strPlayerTwoName, { validators: [Validators.required], updateOn: "change" }),
+      computerName: new FormControl(this.strComputerName, { validators: [Validators.required], updateOn: "change" }),
     });
 
     this.mode$.subscribe(mode => {
@@ -40,9 +44,11 @@ export class ShellComponent implements OnInit {
       this.isUnknown = this.mode === Mode.UNKNOWN;
       this.strPlayerOneName = PLAYER_ONE;
       this.strPlayerTwoName = PLAYER_TWO;
-      this.form.setValue({
+      this.strComputerName = COMPUTER;
+      this.formHuman.setValue({
         playerOneName: this.strPlayerOneName,
         playerTwoName: this.strPlayerTwoName,
+        computerName: this.strComputerName,
       });
     });
   }
@@ -58,21 +64,31 @@ export class ShellComponent implements OnInit {
   }
 
   get playerOneName() {
-    return this.form && (this.form.controls.playerOneName as AbstractControl);
+    return this.formHuman && this.formHuman.controls && (this.formHuman.controls.playerOneName as AbstractControl);
   }
 
   get playerTwoName() {
-    return this.form && (this.form.controls.playerTwoName as AbstractControl);
+    return this.formHuman && this.formHuman.controls && (this.formHuman.controls.playerTwoName as AbstractControl);
+  }
+
+  get computerName() {
+    return this.formHuman && this.formHuman.controls && (this.formHuman.controls.computerName as AbstractControl);
   }
 
   storePlayerNames(formValues: FormGroup, modalId: string) {
     if (formValues.valid) {
-      const { playerOneName: strPlayerOneName, playerTwoName: strPlayerTwoName = "" } = formValues.value;
-      this.strPlayerOneName = strPlayerOneName;
-      this.strPlayerTwoName = !!strPlayerTwoName ? strPlayerTwoName : "Player 2";
+      const {
+        playerOneName: strPlayerOneName = "",
+        playerTwoName: strPlayerTwoName = "",
+        computerName: strComputerName = "",
+      } = formValues.value;
+      this.strPlayerOneName = !!strPlayerOneName ? strPlayerOneName : PLAYER_ONE;
+      this.strPlayerTwoName = !!strPlayerTwoName ? strPlayerTwoName : PLAYER_TWO;
+      this.strComputerName = !!strComputerName ? strComputerName : COMPUTER;
     } else {
       this.strPlayerOneName = PLAYER_ONE;
       this.strPlayerTwoName = PLAYER_TWO;
+      this.strComputerName = COMPUTER;
     }
     this.modalService.close(modalId);
   }
